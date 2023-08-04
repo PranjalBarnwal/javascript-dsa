ae5d63505c6d0f438575acac1a799a993a4b6e9e
Pranjal <happybarnwal007@gmail.com>
2023-08-04 23:48:30 +0530

    constant time

--- isPowerOfTwo.js
+++ isPowerOfTwo.js
@@ -1,16 +1,28 @@
-function isPowerOfTwo(n){
-  if(n<1){
-    return false;
-  }
-  else{
-    while(n>1){
-        if(n%2!==0) return false;
-        else n=n/2;
-    }
-    return true;
-  }
-}
+// function isPowerOfTwo(n){
+//   if(n<1){
+//     return false;
+//   }
+//   else{
+//     while(n>1){
+//         if(n%2!==0) return false;
+//         else n=n/2;
+//     }
+//     return true;
+//   }
+// }
 
-console.log(isPowerOfTwo(8));
+// console.log(isPowerOfTwo(8));
 
 //O(log n)
+
+//--------------------------
+
+function isPowerOfTwo(n) {
+    //using bitwise
+    if (n < 1) return false;
+    return (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOfTwo(256));
+
+//O(1)
